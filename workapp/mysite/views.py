390d1f16c427fc7275326cd5a106640c183fbbee
thimnahindira <thimnahindira@gmail.com>
2020-06-12 17:11:32 -0300

    Changed the HTML for the site.

--- workapp/mysite/views.py
+++ workapp/mysite/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+ 
+def post_list(request): 
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request, 'blog/post_list.html', {})
